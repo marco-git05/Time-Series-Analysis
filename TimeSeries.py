84a5ca2999108916a25c33dd97dabba376b7f44d
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-04-05 22:00:57 -0500

    Add code that generates and plots a noisy time series

--- TimeSeries.py
+++ TimeSeries.py
@@ -1,2 +1,22 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+import math
+
+# Create time vector
+t = np.linspace(0, 10, 100)
+
+# Generate a smooth function
+f_t = t**2
+
+# Add noise
+noise = 10.0 * np.random.randn(len(t))
+noisy_time_series = f_t + noise
+
+# Plot
+plt.figure(figsize=(8,6))
+plt.plot(t, f_t)
+plt.plot(t, noisy_time_series, "b")
+plt.legend()
+plt.title("Time Series With Noise")
+plt.grid(True)
+plt.show()
