0c8e3eeb0f5aa623f32111a97be5a7e5200b3162
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-04-10 22:16:46 -0500

    Add basis functions

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 import numpy as np
 import TimeSeries as ts
-import Integral as 
 
 def main():
     # Create time vector (300 elements from 0 to 3)
     vector = np.linspace(0, 3, 300)
 
     noisy_function = ts.create_noisy_time_series(vector)
+    basis_function = ts.create_basis_function(5, vector)
     ts.plot(vector, noisy_function)
+    ts.plot(vector, basis_function)
 
 main()
--- TimeSeries.py
+++ TimeSeries.py
@@ -1,6 +1,5 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import math
 
 
 def create_noisy_time_series(t):
@@ -22,5 +21,30 @@ def plot(t, function):
     plt.grid(True)
     plt.show()
 
-def create_basis_function(n, t):
-    np.cos(n * t)
+# Cosine basis
+def cosine_basis(n, t):
+    return np.cos(n * t)
+
+# Polynomial-exponential basis
+def poly_exp_basis(n, t):
+    return (t**n) * np.exp(n * t)
+
+def integral(f_x, x):
+    # Vector that starts at 0 and ends at 1 with increments of 0.01
+    x = np.arange(0, 1.01, 0.01)
+
+    # Initialize sum
+    s = 0
+
+    # Add every trapezoid
+    for i in range(1, len(x)):
+        height = (x[i]**2 + x[i-1]**2) / 2
+        base = x[i] - x[i-1]
+        s += height * base
+
+    # Return the result
+    return s
+
+
+def inner_product(f_x, g_x, t):
+    return integral(f_x * g_x, t)
